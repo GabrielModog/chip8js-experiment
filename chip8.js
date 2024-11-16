5f02aafeac268c347aa62dc4e823ce004784fccd
GabrielModog <modog.dev@gmail.com>
2024-11-16 20:06:14 -0300

    fixes temporary display render

--- chip8.js
+++ chip8.js
@@ -1,18 +1,20 @@
-import { FONTSET, START_ADDRESS, VIDEO_HEIGHT, VIDEO_WIDTH } from "./constants.js"
+import { START_ADDRESS, VIDEO_HEIGHT, VIDEO_WIDTH } from "./constants.js"
 import CPU from "./cpu.js"
 import Keyboard from "./keyboard.js"
+import Sound from "./sound.js"
 
 // devices
 const keyboard = new Keyboard()
+const sound = new Sound()
 
 class Chip8 {
   /**
    * @type {CPU}
    */
   cpu
-  
+
   constructor() {
-    this.cpu = new CPU(keyboard)
+    this.cpu = new CPU(keyboard, sound)
 
     this.scale = 10
     this.canvas = document.getElementById("app")
@@ -22,18 +24,12 @@ class Chip8 {
     this.canvas.height = VIDEO_HEIGHT * this.scale
   }
 
-  loadFontsetInMemory() {
-    for(let i = 0; i < FONTSET.length; i++) {
-      this.cpu.memory[i] = FONTSET[i]
-    }
-  }
-
   /**
    * loadRomBufferInMemory(buffer)
    * @param {Uint8Array} buffer 
    */
   loadRomBufferInMemory(buffer) {
-    for(let loc = 0; loc < buffer.length; loc++) {
+    for (let loc = 0; loc < buffer.length; loc++) {
       this.cpu.memory[START_ADDRESS + loc] = buffer[loc]
     }
   }
@@ -42,7 +38,7 @@ class Chip8 {
    * fetchRom(romName)
    * @param {string} romName
    */
-  async fetchRom(romName = "corax.ch8") {
+  async fetchRom(romName = "chip8picture.ch8") {
     const response = await fetch(`roms/${romName}`)
     const data = await response.arrayBuffer()
     this.loadRomBufferInMemory(new Uint8Array(data))
@@ -50,27 +46,33 @@ class Chip8 {
 
   tempRender() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
-    for (let i = 0; i < VIDEO_WIDTH * VIDEO_HEIGHT; i++) {
-      let x = (i % VIDEO_WIDTH) * this.scale
-      let y = Math.floor(i / VIDEO_WIDTH) * this.scale
-      if (this.cpu.video[i] > 0) {
-        this.ctx.fillStyle = "#000"
-        this.ctx.fillRect(x, y, this.scale, this.scale)
+    for (let x = 0; x < VIDEO_WIDTH; x++) {
+      for (let y = 0; y < VIDEO_HEIGHT; y++) {
+        const pixel = this.cpu.video[x + y * VIDEO_WIDTH]
+        if (pixel) {
+          this.ctx.fillStyle = "#000"
+          this.ctx.fillRect(
+            x * this.scale,
+            y * this.scale,
+            this.scale,
+            this.scale
+          )
+        }
       }
     }
   }
 
   init() {
-    this.loadFontsetInMemory()
+    this.cpu.loadFontsetInMemory()
     this.fetchRom()
   }
 
   run() {
+    // TODO: remove setInterval and use requestAnimationFrame instead
     setInterval(() => {
-      // console.log(this.cpu.video)
       this.cpu.cycle()
       this.tempRender()
-    }, 1000 / 600)
+    }, 50)
   }
 }
 
