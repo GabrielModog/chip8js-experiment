af35880f0ddbc704bde49386488db4efc3ecdeb9
GabrielModog <modog.dev@gmail.com>
2024-11-17 23:59:09 -0300

    refactor server

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 import { existsSync } from "fs"
 
+const mainFolder = "public"
+
 const server = Bun.serve({
   port: 3000,
   async fetch(request) {
     const url = new URL(request.url)
-    if (url.pathname === "/") return new Response(Bun.file("index.html"))
-    if (url.pathname === "/main.js") return new Response(Bun.file("main.js"))
+    // check main files
+    if (url.pathname === "/") 
+      return new Response(Bun.file(`${mainFolder}/index.html`))
+    if (url.pathname === "/main.js")
+      return new Response(Bun.file(`${mainFolder}/main.js`))
     // check roms files
     if (url.pathname.startsWith("/roms/")) {
-      const filePath = `.${url.pathname}`
+      let filePath = `.${url.pathname}`
+      filePath = filePath.replace("/roms/", `/${mainFolder}/roms/`)
       if (existsSync(filePath)) 
-        return new Response(Bun.file(filePath))
-    }
-    // check javascript files
-    if (url.pathname.startsWith("/src/")) {
-      const filePath = `.${url.pathname}`
-      if (existsSync(filePath))
         return new Response(Bun.file(filePath))
     }
     return new Response("Not Found", { status: 404 })
