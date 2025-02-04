6c7a303147d989c478bf119dd0834ec27185bf35
GabrielModog <modog.dev@gmail.com>
2025-02-04 04:39:40 -0300

    split instructions

--- src/cpu.js
+++ src/cpu.js
@@ -1,7 +1,8 @@
 import { VIDEO_WIDTH, VIDEO_HEIGHT, START_ADDRESS, FONTSET, FONT_START_ADDRESS } from "./constants.js"
-import Display from "./display.js"
-import Keyboard from "./keyboard.js"
-import Sound from "./sound.js"
+
+/**   @import Display from "./display.js"     */
+/**   @import Keyboard from "./keyboard.js"   */
+/**   @import Sound from "./sound.js"         */
 
 export default class CPU {
   /**
@@ -35,14 +36,14 @@ export default class CPU {
   }
 
   cycle() {
-     let opcode = (this.memory[this.pc] << 8 | this.memory[this.pc + 1])
-     this.instructions(opcode)
-     if (this.delayTimer > 0) {
-       this.delayTimer -= 1
-     }
-     if (this.soundTimer > 0) {
-       this.soundTimer -= 1
-     }
+    let opcode = (this.memory[this.pc] << 8 | this.memory[this.pc + 1])
+    this.instructions(opcode)
+    if (this.delayTimer > 0) {
+      this.delayTimer -= 1
+    }
+    if (this.soundTimer > 0) {
+      this.soundTimer -= 1
+    }
 
     if (this.soundTimer > 0) {
       this.sound.play()
@@ -93,15 +94,10 @@ export default class CPU {
         this.op_0(opcode)
       } break
       case 0x1000: {
-        this.pc = nnn
+        this.op_1(nnn)
       } break
       case 0x2000: {
-        if(this.stack.length > 0xf) {
-          console.error("Stack Overflow")
-          return
-        } 
-        this.stack.push(this.pc)
-        this.pc = nnn
+        this.op_2(nnn)
       } break
       case 0x3000: {
         if (this.registers[x] === bytekk) {
@@ -192,7 +188,7 @@ export default class CPU {
           let pixel = this.memory[this.i + row]
           for (let col = 0; col < width; col++) {
             if ((pixel & (msb >> col)) !== 0) {
-              const xx = (xReg + col) 
+              const xx = (xReg + col)
               const yy = (yReg + row)
               const idx = xx + (yy * VIDEO_WIDTH)
               if (this.video[idx] === 1) {
@@ -268,15 +264,202 @@ export default class CPU {
   }
 }
 
-CPU.prototype.op_0 = function(opcode){
+CPU.prototype.op_0 = function(opcode) {
   switch (opcode) {
     case 0x00e0: {
       this.clearDisplay()
     } break
     case 0x00ee: {
-      if(this.stack < 0x0) return console.error("Stack Underflow")
+      if (this.stack < 0x0) return console.error("Stack Underflow")
       this.pc = this.stack.pop()
     } break
+  }
+}
+
+CPU.prototype.op_1 = function(nnn) {
+  this.pc = nnn
+}
+
+CPU.prototype.op_2 = function(nnn) {
+  if (this.stack.length > 0xf) {
+    console.error("Stack Overflow")
+    return
+  }
+  this.stack.push(this.pc)
+  this.pc = nnn
+}
+
+CPU.prototype.op_3 = function(vx, bytekk) {
+  if (this.registers[vx] === bytekk) {
+    this.increment_pc()
+  }
+}
+
+CPU.prototype.op_4 = function(vx, bytekk) {
+  if (this.registers[vx] !== bytekk) {
+    this.increment_pc()
+  }
+}
+
+CPU.prototype.op_5 = function(vx, vy) {
+  if (this.registers[vx] === this.registers[vy]) {
+    this.increment_pc()
+  }
+}
+
+CPU.prototype.op_6 = function(vx, bytekk) {
+  this.registers[vx] = bytekk
+}
+
+CPU.prototype.op_7 = function(vx, bytekk) {
+  this.registers[vx] += bytekk
+}
+
+CPU.prototype.op_8 = function(opcode, vx, vy) {
+  const mode = (opcode & 0xf)
+  switch (mode) {
+    case 0x0: {
+      this.registers[vx] = this.registers[vy]
+    } break
+    case 0x1: {
+      this.registers[vx] |= this.registers[vy]
+    } break
+    case 0x2: {
+      this.registers[vx] &= this.registers[vy]
+    } break
+    case 0x3: {
+      this.registers[vx] ^= this.registers[vy]
+    } break
+    case 0x4: {
+      this.registers[0xf] = 0
+      const sum = this.registers[vx] + this.registers[vy]
+      if (sum > 0xff) {
+        this.registers[0xf] = 1
+      }
+      this.registers[vx] = sum
+    } break
+    case 0x5: {
+      this.registers[0xf] = 0
+      if (this.registers[vx] > this.registers[vy]) {
+        this.registers[0xf] = 1
+      }
+      this.registers[vx] -= this.registers[vy]
+    } break
+    case 0x6: {
+      this.registers[0xf] = this.registers[vx] & 0x1
+      this.registers[vx] >>= 1
+    } break
+    case 0x7: {
+      this.registers[0xf] = 0
+      if (this.registers[vy] > this.registers[vx]) {
+        this.registers[0xf] = 1
+      }
+      this.registers[vx] = this.registers[vy] - this.registers[vx]
+    } break
+    case 0xe: {
+      this.registers[0xf] = (this.registers[vx] & 0x80) >> 7
+      this.registers[vx] <<= 1
+    } break
+  }
+}
+
+CPU.prototype.op_9 = function(vx, vy) {
+  if (this.registers[vx] !== this.registers[vy]) {
+    this.increment_pc()
+  }
+}
+
+CPU.prototype.op_a = function(nnn) {
+  this.i = nnn
+}
+
+CPU.prototype.op_b = function(nnn) {
+  this.pc = this.registers[0] + nnn
+}
+
+CPU.prototype.op_c = function(vx, bytekk) {
+  const rand = this.rand()
+  this.registers[vx] = rand & bytekk
+}
+
+CPU.prototype.op_d = function(xReg, yReg, width, height, msb) {
+  this.registers[0xf] = 0 // set VF = collision
+  for (let row = 0; row < height; row++) {
+    let pixel = this.memory[this.i + row]
+    for (let col = 0; col < width; col++) {
+      if ((pixel & (msb >> col)) !== 0) {
+        const xx = (xReg + col)
+        const yy = (yReg + row)
+        const idx = xx + (yy * VIDEO_WIDTH)
+        if (this.video[idx] === 1) {
+          this.registers[0xf] = 1
+        }
+        this.video[idx] ^= 1
+      }
+    }
+  }
+}
+
+CPU.prototype.op_e = function(opcode, vx) {
+  const mode = (opcode & 0xff)
+  switch (mode) {
+    case 0x9e: {
+      if (this.keyboard.isKeyPressed(this.registers[vx])) {
+        this.increment_pc()
+      }
+    } break
+    case 0xa1: {
+      if (!this.keyboard.isKeyPressed(this.registers[vx])) {
+        this.increment_pc()
+      }
+    } break
+  }
+}
+
+CPU.prototype.op_f = function(opcode, vx) {
+  const mode = (opcode & 0xff)
+  switch (mode) {
+    case 0x07: {
+      this.registers[vx] = this.delayTimer
+    } break
+    case 0x0a: {
+      this.paused = true
+      this.keyboard.nextKeyCallback = (key) => {
+        this.registers[vx] = key
+        this.paused = false
+      }
+    } break
+    case 0x15: {
+      this.delayTimer = this.registers[vx]
+    } break
+    case 0x18: {
+      this.soundTimer = this.registers[vx]
+    } break
+    case 0x1e: {
+      this.i += this.registers[vx]
+    } break
+    case 0x29: {
+      this.i = this.registers[vx] * 5
+    } break
+    case 0x33: {
+      let value = this.registers[vx]
+      this.memory[this.i] = Math.floor(value / 100)
+      this.memory[this.i + 1] = Math.floor((value % 100) / 10)
+      this.memory[this.i + 2] = Math.floor(value % 10)
+    } break
+    case 0x55: {
+      for (let registerIndex = 0; registerIndex <= vx; registerIndex++) {
+        this.memory[this.i + registerIndex] = this.registers[registerIndex]
+      }
+    } break
+    case 0x65: {
+      for (let registerIndex = 0; registerIndex <= vx; registerIndex++) {
+        this.registers[registerIndex] = this.memory[this.i + registerIndex]
+      }
+    } break
+    default:
+      console.error(`Unknow OPCODE: 0x${opcode.toString(16)}`)
+      break
   }
 }
 
